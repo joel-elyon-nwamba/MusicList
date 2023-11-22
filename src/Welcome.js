5f7190d5638691e2ae475dfe9446f18f4bb00962
joel-nwamba <joel.elyon.nwamba@gmail.com>
2023-11-21 22:32:36 -0500

    adding welcome page

---
+++ src/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.jpg'
+function Welcome() {
+  return(
+    <div>
+      <h1>Welcome to Music Influence</h1>
+      <img src="./logo.jpg" alt="music Logo" />
+      
+    </div>
+  )
+}
+
+export default Welcome;
