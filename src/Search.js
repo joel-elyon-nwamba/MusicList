1ca00d17b42c34546f8113a572f4d05afd8b36f3
joel-nwamba <joel.elyon.nwamba@gmail.com>
2023-11-26 00:35:53 -0500

    added css style for each component

--- src/Search.js
+++ src/Search.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react"
-
+import "./Search.css"
 function Search() {
   const [search, setSearch] = useState("");
         function handleInput(event) {
           setSearch(event.target.value)
         }
   return(
-    <div>
+    <div className ="container">
       <form>
         <input type="text"
         value={search}
         onChange={handleInput}
          />
-        <button>Submit</button>
+        <button className="class-btn">Submit</button>
       </form>
     </div>
   )
